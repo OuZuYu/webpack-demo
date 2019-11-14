6e11ec2978462762cce485d4f6ed4c7fc1db9cb3
ouzuyu <oupenguin@gmail.com>
2019-11-14 15:38:34 +0800

    2-生成html文件 使用html-webpack-plugin生成html

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index',
   output: {
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html'
+    })
+  ]
 }
